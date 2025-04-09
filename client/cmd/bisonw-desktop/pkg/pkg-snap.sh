2e2f9639a3396f545d551f3467da452e82489005
Peter Banik <peter@froggle.org>
2025-04-09 11:36:06 +0000

    fix gh workflow

--- client/cmd/bisonw-desktop/pkg/pkg-snap.sh
+++ client/cmd/bisonw-desktop/pkg/pkg-snap.sh
@@ -6,9 +6,5 @@ SCRIPT_DIR=$(dirname "$0")
 
 source $SCRIPT_DIR/common.sh
 
-SNAPCRAFT_YML_IN=snap/local/snapcraft.yaml.in
-SNAPCRAFT_YML=snap/snapcraft.yaml
-sed -e "s/\$VERSION/$VER/g" \
-    -e "s/\$DEB_NAME/$DEB_NAME/g" "$SNAPCRAFT_YML_IN" > "$SNAPCRAFT_YML"
-
+pkg/prepare-snap.sh
 snapcraft --verbose --output $BUILD_DIR/
